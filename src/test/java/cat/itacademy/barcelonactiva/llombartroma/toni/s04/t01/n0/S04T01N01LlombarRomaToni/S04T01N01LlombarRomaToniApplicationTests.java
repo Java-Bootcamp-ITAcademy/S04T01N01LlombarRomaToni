92badd423f6fe3f2cde83c7f0a5055ea502c47df
unknown <antoni.llombart@gmail.com>
2023-05-08 12:37:38 +0200

    Initial commit

---
+++ src/test/java/cat/itacademy/barcelonactiva/llombartroma/toni/s04/t01/n0/S04T01N01LlombarRomaToni/S04T01N01LlombarRomaToniApplicationTests.java
@@ -0,0 +1,13 @@
+package cat.itacademy.barcelonactiva.llombartroma.toni.s04.t01.n0.S04T01N01LlombarRomaToni;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S04T01N01LlombarRomaToniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
